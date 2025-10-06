cb3305d17fc85ec131260e822444c1991b26c2cd
LProjects-git <linabel02@hotmail.fr>
2025-10-06 16:45:47 +0200

    Ajout

--- client/contact-manager/src/services/api.js
+++ client/contact-manager/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
+import axios from 'axios';
+
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true
 });
+export default apiClient;
 
 export const register = (userInfo) =>
   apiClient.post('/auth/register', userInfo);
